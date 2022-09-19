37df025b8913c5252c71ef8013eb76f815c66452
zhaoxuan <xuanzhao@88.com>
2022-09-19 16:42:45 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,56 @@
+export default {
+    title: 'Xuan Zhao',
+    description: '赵旋的简历 | xuan zhao',
+    lang: 'zh-CN',
+
+    head: [
+        ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi' }],
+    ],
+
+    lastUpdated: true,
+    themeConfig: {
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/0xbul1' }
+        ],
+        nav: [
+            // NavbarItem
+            // {
+            //     text: '首页',
+            //     link: '/',
+            // },
+            // {
+            //     text: '规则',
+            //     link: '/rule/',
+            // },
+            // {
+            //     text: '例子',
+            //     link: '/example/parallelcdn',
+            // },
+            // {
+            //     text: '拓展',
+            //     link: '/ext/engine',
+            // },
+            // {
+            //     text: 'Q群',
+            //     link: 'https://jq.qq.com/?_wv=1027&k=rAcnhzqK',
+            // }
+        ],
+        sidebar: [
+            {
+                text: '项目',
+                items: [
+                    { text: '活力钱包', link: '/project/1' },
+                    { text: '企业微信应用后台', link: '/project/2' },
+                    { text: '慢牛助手/财富雷达', link: '/project/3' },
+                    { text: '早报编辑器', link: '/project/4' }
+                ]
+            },
+            {
+                text: '其他',
+                items: [
+                    { text: '联系我', link: '/contact/' }
+                ]
+            }
+        ]
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,30 @@
+---
+layout: home
+title: Xuan Zhao
+titleTemplate: 找测试工作的简历 | Portfolio for FrontEnd
+hero:
+  name: 找工作 | 前端转测试
+  text: 技术栈：Vue + TS + GraphQl
+  tagline: 赵旋的简历
+  actions:
+   - theme: brand
+     text: 看项目
+     link: /project/1
+   - theme: alt
+     text: github
+     link: https://github.com/0xbul1
+
+features:
+- icon: 👨‍🎓
+  title: 学历：全日制本科，学信网可查
+  details: 教育经历：<br>2015.9 - 2019.7：<a style='color:#42b883' target="_blank" href="https://www.wxc.edu.cn/">皖西学院</a><br><br><br>Tips:<br><span style='color:red;font-size:12px' >本人保证与您的一切沟通，均有据可查，愿意承担任何因欺诈造成的后果与法律责任！</span>
+- icon: 🛠️
+  title: 状态：已离职
+  details: 工作经历：<br>2018.7 - 2020.4：<a style='color:#42b883' target="_blank" href="http://www.yuanmaokj.com/">安徽罗阁科技有限公司</a><br>2020.6 - 2022-9：<a style='color:#42b883' target="_blank" href="https://fantaiai.com/">上海钒钛智能科技有限公司</a><br><br><a style='color:#42b883' target="_blank" href="https://fantaiai.com/">下载纸质简历</a><br><br>支持电话面试，现场面试
+- icon: ⚡️
+  title: 项目：详细请点击看项目
+  details: 项目经历：<br>2018.7 - 2020.4：实习<br>2020.6 - 2022.9：<br><a style='color:#42b883' href="/start">1、活力钱包</a><br><a style='color:#42b883' href="/start">2、企业微信应用后台</a><br><a style='color:#42b883' href="/start">3、慢牛助手/财富雷达</a><br><a style='color:#42b883' href="/start">4、早报编辑器</a><br><br><a style='color:#42b883' href="/start">下载纸质简历</a><br>支持电话面试，现场面试
+- icon: ✌️
+  title: 联系：您可通过以下方式
+  details: 电话：<a style='color:#42b883' target="_blank" href="tel:18888888888">1XX-XXXX-XXXX</a><br><br>Email：<a style='color:#42b883' target="_blank" href="mailto:xuanzhao@88.com">xuanzhao@88.com</a>
+---
