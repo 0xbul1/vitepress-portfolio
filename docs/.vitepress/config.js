870922a744fabbf2c1456bd2ab28f313bbac1043
zhaoxuan <xuanzhao@88.com>
2022-09-23 17:29:48 +0800

    add

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,7 +6,6 @@ export default {
     head: [
         ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi' }],
     ],
-    base: '/vitepress-portfolio/',
     lastUpdated: true,
     themeConfig: {
         socialLinks: [
